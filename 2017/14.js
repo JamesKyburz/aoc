9ce52b6ec2aaef70ccd7ad2a410d6fe9668fe8e9
James Kyburz <james.kyburz@gmail.com>
2017-12-16 17:05:55 +0100

    a bit better

--- 2017/14.js
+++ 2017/14.js
@@ -4,7 +4,7 @@ for (const { input, expected, solution } of [
   { input: 'flqrgnkx', expected: 8108, solution: bitCount },
   { input: 'ffayrhll', expected: 8190, solution: bitCount },
   { input: 'flqrgnkx', expected: 1242, solution: groupCount },
-  { input: 'ffayrhll', expected: 1242, solution: groupCount }
+  { input: 'ffayrhll', expected: 1134, solution: groupCount }
 ]) {
   const actual = solution(input)
   if (actual !== expected) console.error(`${actual} !== ${expected}`)
@@ -32,45 +32,38 @@ function groupCount (input) {
       (sum, chr) => sum + pad4(parseInt(chr, 16).toString(2)),
       ''
     )
-    for (const x of size) {
-      matrix[[x, y]] = [+bits[x]]
-    }
+    for (const x of size) matrix[[x, y]] = [+bits[x]]
   }
-  let count = 0
   let currentGroup = 0
-  let groupCount = 0
-  let loopsPostSame = 0
-  while (true) {
-    for (const y of size) {
-      for (const x of size) {
-        const cells = [...connected(matrix, x, y)]
-        if (cells.length) {
-          const groups = cells.map((x) => x[1]).filter(Boolean)
-          if (!groups.length) groups.push(++currentGroup)
-          for (const cell of cells) cell[1] = Math.min(...groups)
-        }
+  for (const y of size) {
+    for (const x of size) {
+      const cells = [...connected()(matrix, x, y)]
+      if (cells.length) {
+        const groups = cells.map((x) => x[1]).filter(Boolean)
+        if (!groups.length) groups.push(++currentGroup)
+        for (const cell of cells) cell[1] = Math.min(...groups)
       }
     }
-    const groups = {}
-    for (const y of size) {
-      for (const x of size) {
-        if (matrix[[x, y]][0]) groups[matrix[[x, y]][1]] = 1
-      }
-    }
-    const newGroupCount = Object.keys(groups).length
-    if (newGroupCount === groupCount) {
-      loopsPostSame++
-      if (loopsPostSame === 10) return groupCount
-    }
-    groupCount = newGroupCount
   }
+  const groups = {}
+  for (const y of size) {
+    for (const x of size) {
+      if (matrix[[x, y]][0]) groups[matrix[[x, y]][1]] = 1
+    }
+  }
+  return Object.keys(groups).length
 }
 
-function * connected (matrix, x, y) {
-  if (!matrix[[x, y]][0]) return
-  yield matrix[[x, y]]
-  if (matrix[[x + 1, y]] && matrix[[x + 1, y]][0]) yield matrix[[x + 1, y]]
-  if (matrix[[x - 1, y]] && matrix[[x - 1, y]][0]) yield matrix[[x - 1, y]]
-  if (matrix[[x, y + 1]] && matrix[[x, y + 1]][0]) yield matrix[[x, y + 1]]
-  if (matrix[[x, y - 1]] && matrix[[x, y - 1]][0]) yield matrix[[x, y - 1]]
+function connected () {
+  const visited = {}
+  return function * steps (matrix, x, y) {
+    if (!matrix[[x, y]][0]) return
+    if (visited[[x, y]]) return
+    visited[[x, y]] = 1
+    yield matrix[[x, y]]
+    if (matrix[[x + 1, y]] && matrix[[x + 1, y]][0]) yield * steps(matrix, x + 1, y)
+    if (matrix[[x - 1, y]] && matrix[[x - 1, y]][0]) yield * steps(matrix, x - 1, y)
+    if (matrix[[x, y + 1]] && matrix[[x, y + 1]][0]) yield * steps(matrix, x, y + 1)
+    if (matrix[[x, y - 1]] && matrix[[x, y - 1]][0]) yield * steps(matrix, x, y - 1)
+  }
 }
