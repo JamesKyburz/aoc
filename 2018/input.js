961d323b87fc7e1d8556680939b9bf471f50cc52
James Kyburz <james.kyburz@gmail.com>
2018-12-02 11:23:22 +0100

    day 1 + input resolver

--- 2018/input.js
+++ 2018/input.js
@@ -1,10 +1,15 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
-const session = fs.readFileSync('./session', 'utf-8')
+const path = require('path')
+
+const session = encodeURIComponent(fs.readFileSync(path.join(__dirname, '.session'), 'utf-8'))
 
 module.exports = async relative => {
-  const res = await fetch('https://adventofcode.com/2018/day/' + relative)
+  const res = await fetch('https://adventofcode.com/2018/day/' + relative, {
+    method: 'GET',
+    headers: {
+      cookie: `session=${session};`
+    }
+  })
   return res.text()
 }
-
-
