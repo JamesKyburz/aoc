ebdfcb9b631c0e6f306767048a953dff7842d3c7
James Kyburz <james.kyburz@gmail.com>
2018-12-02 16:21:46 +0100

    day 2 part 2

--- 2018/2.js
+++ 2018/2.js
@@ -2,7 +2,7 @@ const { test } = require('tap')
 const { lines, input } = require('./helpers')
 
 test('day 2', async t => {
-  t.plan(1)
+  t.plan(2)
   const ids = lines(await input('2/input'))
 
   let ii = 0
@@ -20,4 +20,27 @@ test('day 2', async t => {
     if (values.includes(3)) iii++
   }
   t.equals(ii * iii, 6723)
+
+  let match
+
+  for (const [...i] of ids) {
+    if (match) break
+    const positions = [...Array(i.length)].map((x, i) => i)
+    for (const [...j] of ids) {
+      let diff = 0
+      let lastPos = 0
+      for (const pos of positions) {
+        if (i[pos] !== j[pos]) {
+          diff++
+          lastPos = pos
+        }
+      }
+      if (diff === 1) {
+        i.splice(lastPos, 1)
+        match = i.join('')
+        break
+      }
+    }
+  }
+  t.equals(match, 'prtkqyluiusocwvaezjmhmfgx')
 })
