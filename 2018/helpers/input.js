78cbd7be9e2dfe301b6f07316f3d9105e6906897
James Kyburz <james.kyburz@gmail.com>
2018-12-02 15:06:54 +0100

    memoize input

--- 2018/helpers/input.js
+++ 2018/helpers/input.js
@@ -5,11 +5,16 @@ const path = require('path')
 const session = encodeURIComponent(fs.readFileSync(path.join(__dirname, '.session'), 'utf-8'))
 
 module.exports = async relative => {
+  try {
+    return fs.readFileSync(path.join(__dirname, '.input' + relative.replace(/\//g, '-')), 'utf-8')
+  } catch {}
   const res = await fetch('https://adventofcode.com/2018/day/' + relative, {
     method: 'GET',
     headers: {
       cookie: `session=${session};`
     }
   })
-  return res.text()
+  const result = await res.text()
+  fs.writeFileSync(path.join(__dirname, '.input' + relative.replace(/\//g, '-')), result)
+  return result
 }
