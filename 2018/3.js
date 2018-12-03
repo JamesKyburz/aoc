09fc8acda55f351791d2952b0abf38c81d39fed0
James Kyburz <james.kyburz@gmail.com>
2018-12-03 19:32:46 +0100

    dog slow and more functional day 3

--- 2018/3.js
+++ 2018/3.js
@@ -4,23 +4,27 @@ const { input, lines, sequence } = require('./helpers')
 test('day 3', async t => {
   t.plan(2)
   const counter = {}
+  const parse = line =>
+    [...line.match(/^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)].map(Number)
   const instructions = lines(await input('3/input'))
-    .map(line =>
-      [...line.match(/^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)].map(Number)
-    )
+    .map(parse)
     .map(([, id, x, y, width, height]) => ({
       id,
       matrix: sequence(height)
-        .map(iy => sequence(width).map(ix => [ix, iy]))
-        .reduce((sum, item) => {
-          for (const [ix, iy] of item) {
-            const pos = `${x + ix}x${y + iy}`
-            sum[pos] = 0
-            counter[pos] = counter[pos] || 0
-            counter[pos]++
-          }
-          return sum
-        }, {})
+        .map(iy => sequence(width).map(ix => `${x + ix}x${y + iy}`))
+        .reduce(
+          (sum, item) => ({
+            ...sum,
+            ...item.reduce(
+              (sum, pos) => ({
+                ...sum,
+                [pos]: (counter[pos] = (counter[pos] || 0) + 1)
+              }),
+              {}
+            )
+          }),
+          {}
+        )
     }))
 
   const overlap = Object.values(counter).filter(x => x > 1).length
