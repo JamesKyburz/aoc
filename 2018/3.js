1e37aa6e164d9817e77753635569104b09962e73
James Kyburz <james.kyburz@gmail.com>
2018-12-04 07:48:01 +0100

    working 3 again

--- 2018/3.js
+++ 2018/3.js
@@ -4,20 +4,23 @@ const { input, lines, sequence } = require('./helpers')
 test('day 3', async t => {
   t.plan(2)
   const counter = {}
-  const parse = line =>
-    [...line.match(/^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)].map(Number)
   const instructions = lines(await input('3/input'))
-    .map(parse)
+    .map(line =>
+      [...line.match(/^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/)].map(Number)
+    )
     .map(([, id, x, y, width, height]) => ({
       id,
       matrix: sequence(height)
-        .map(iy => sequence(width).map(ix => `${x + ix}x${y + iy}`))
+        .map(iy => sequence(width).map(ix => [ix, iy]))
         .reduce((sum, item) => {
-          for (const pos of item) {
-            sum[pos] = counter[pos] = (counter[pos] || 0) + 1
+          for (const [ix, iy] of item) {
+            const pos = `${x + ix}x${y + iy}`
+            sum[pos] = 0
+            counter[pos] = counter[pos] || 0
+            counter[pos]++
           }
           return sum
-        })
+        }, {})
     }))
 
   const overlap = Object.values(counter).filter(x => x > 1).length
