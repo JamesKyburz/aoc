46779d6659606aa410e093d8a21256bdb76b5df0
James Kyburz <james.kyburz@gmail.com>
2018-12-12 08:28:35 +0100

    all broken rtm

--- 2018/7.js
+++ 2018/7.js
@@ -3,7 +3,11 @@ const { input, lines } = require('./helpers')
 
 test('day 7', async t => {
   t.plan(1)
-  const functions = lines(`Step C must be finished before step A can begin.
+  const steps = lines(await input('7/input'))
+    .map(line => line.match(/[A-Z]/g).slice(1))
+    .sort((a, b) => (a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0))
+
+  const xsteps = lines(`Step C must be finished before step A can begin.
 Step C must be finished before step F can begin.
 Step A must be finished before step B can begin.
 Step A must be finished before step D can begin.
@@ -12,54 +16,52 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.
 `)
     .map(line => line.match(/[A-Z]/g).slice(1))
-    .reduce((sum, [parent, child]) => {
-      if (!sum[parent]) sum[parent] = { children: [], name: parent, after: [] }
-      if (!sum[parent].children.includes(child)) {
-        sum[parent].children.push(child)
-        sum[parent].children.sort()
-      }
-      if (!sum[child]) sum[child] = { name: child, children: [], after: [] }
-      sum[child].after.push(parent)
-      return sum
-    }, {})
-  const steps = new Set()
-  const canRun = name => {
-    if (!steps.has(name) && functions[name].after.every(x => steps.has(x))) {
-      return true
+    .sort((a, b) => (a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0))
+
+  const completed = new Set()
+  const waiting = new Set()
+  const toNode = x => x.split(/,/)
+  const xfree = () =>
+    [...waiting]
+      .map(toNode)
+      .concat(steps)
+      .find(([l, r]) => !steps.find(([_, x]) => x === l)) ||
+    steps.find(([l, r]) => !steps.find(([_, x]) => x === l))
+  const canRun = name => !steps.find(x => x[1] === name) && !completed.has(name)
+  const free = (last = []) =>
+    [...waiting]
+      .map(toNode)
+      .concat(steps)
+      .sort((a, b) => (a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0))
+      .find(x => x[0] !== last[0] && x[1] !== last[1] && x.some(canRun))
+
+  const walk = node => {
+    if (!node) return
+    if (typeof node === 'string') node = toNode(node)
+    const stepIndex = steps.findIndex(x => x[0] === node[0] && x[1] === node[1])
+    if (stepIndex !== -1) steps.splice(stepIndex, 1)
+    if (node.every(node => canRun(node) || completed.has(node))) {
+      waiting.delete(node + '')
+    } else {
+      waiting.add(node + '')
     }
-  }
-  const next = () => {
-    for (const i of Object.keys(functions)) {
-      if (canRun(i)) return functions[i]
-      for (const j of functions[i].children) {
-        if (canRun(j)) return functions[j]
-        for (const k of functions[j].children) {
-          if (canRun(k)) return functions[k]
-          for (const l of functions[k].children) {
-            if (canRun(l)) return functions[l]
-            for (const m of functions[l].children) {
-              if (canRun(m)) return functions[m]
-              for (const n of functions[m].children) {
-                if (canRun(n)) return functions[n]
-                for (const o of functions[n].children) {
-                  if (canRun(o)) return functions[o]
-                  for (const p of functions[o].children) {
-                    if (canRun(p)) return functions[p]
-                  }
-                }
-              }
-            }
-          }
-        }
+    for (const name of node) {
+      if (canRun(name)) {
+        completed.add(name)
+        const pending = steps
+          .filter(x => x[0] === node[1])
+          .sort((a, b) => (a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : 0))
+        for (const node of pending) walk(node)
       }
     }
   }
 
-  while (true) {
-    const fn = next()
-    if (!fn) break
-    steps.add(fn.name)
+  let node
+  while (steps.length + waiting.size) {
+    walk((node = free(node)))
+    walk(free())
+    console.log([...completed])
   }
 
-  t.equals('CABDFE', [...steps].join(''))
+  t.equals([...completed].join(''), 'CABDFGE')
 })
