e4abfd98b395a05dabc995fc28871cbd92464500
James Kyburz <james.kyburz@gmail.com>
2020-12-05 16:02:20 +0000

    day 5 another way

--- 2020/day-5-2.js
+++ 2020/day-5-2.js
@@ -7,18 +7,17 @@ const diff = ids.map((id, i) => ids[i + 1] - id)
 console.log(ids[diff.findIndex(x => x !== 1)] + 1)
 
 function id (input) {
-  const row = find(0, 127, [...input].slice(0, 7))
-  const col = find(0, 7, [...input].slice(7))
+  const row = find([...Array(128).keys()], [...input].slice(0, 7))
+  const col = find([...Array(8).keys()], [...input].slice(7))
   return row * 8 + col
 }
 
-function find (min, max, steps) {
+function find (array, steps) {
+  if (array.length === 1) return array[0]
   const step = steps.shift()
   if (step === 'F' || step === 'L') {
-    return find(min, ((max + 1 + min) / 2) - 1, steps)
+    return find(array.slice(0, array.length / 2), steps)
   } else if (step === 'B' || step === 'R') {
-    return find(((max + 1 + min) / 2), max, steps)
-  } else {
-    return min
+    return find(array.slice(array.length / 2), steps)
   }
 }
--- 2020/day-5.js
+++ 2020/day-5.js
@@ -3,18 +3,17 @@ const input = require('./read')(__filename)
 console.log(Math.max(...[...input].map(id)))
 
 function id (input) {
-  const row = find(0, 127, [...input].slice(0, 7))
-  const col = find(0, 7, [...input].slice(7))
+  const row = find([...Array(128).keys()], [...input].slice(0, 7))
+  const col = find([...Array(8).keys()], [...input].slice(7))
   return row * 8 + col
 }
 
-function find (min, max, steps) {
+function find (array, steps) {
+  if (array.length === 1) return array[0]
   const step = steps.shift()
   if (step === 'F' || step === 'L') {
-    return find(min, ((max + 1 + min) / 2) - 1, steps)
+    return find(array.slice(0, array.length / 2), steps)
   } else if (step === 'B' || step === 'R') {
-    return find(((max + 1 + min) / 2), max, steps)
-  } else {
-    return min
+    return find(array.slice(array.length / 2), steps)
   }
 }
