25387aafc473ac82a25886d924ec6e1b9a47c062
James Kyburz <james.kyburz@gmail.com>
2020-12-05 16:52:35 +0000

    day 5 third option

--- 2020/day-5-2.js
+++ 2020/day-5-2.js
@@ -1,7 +1,5 @@
 const input = require('./read')(__filename)
-
 const ids = [...input].map(id).sort()
-
 const diff = ids.map((id, i) => ids[i + 1] - id)
 
 console.log(ids[diff.findIndex(x => x !== 1)] + 1)
--- 2020/day-5.js
+++ 2020/day-5.js
@@ -3,17 +3,16 @@ const input = require('./read')(__filename)
 console.log(Math.max(...[...input].map(id)))
 
 function id (input) {
-  const row = find([...Array(128).keys()], [...input].slice(0, 7))
-  const col = find([...Array(8).keys()], [...input].slice(7))
+  const row = find(0, 127, [...input].slice(0, 7))
+  const col = find(0, 7, [...input].slice(7))
   return row * 8 + col
 }
 
-function find (array, steps) {
-  if (array.length === 1) return array[0]
-  const step = steps.shift()
-  if (step === 'F' || step === 'L') {
-    return find(array.slice(0, array.length / 2), steps)
-  } else if (step === 'B' || step === 'R') {
-    return find(array.slice(array.length / 2), steps)
-  }
+function find (min, max, steps) {
+  const reduce = min + ((max - min) >> 1)
+  if (min === max) return min
+  return find(
+    ...(/F|L/.test(steps.shift()) ? [min, reduce] : [reduce + 1, max]),
+    steps
+  )
 }
