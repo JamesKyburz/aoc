4c8c73b509c3834a53611507e6af090f0c5e01b6
James Kyburz <james.kyburz@gmail.com>
2020-12-05 17:16:47 +0000

    day 5 haha

--- 2020/day-5-2.js
+++ 2020/day-5-2.js
@@ -4,18 +4,6 @@ const diff = ids.map((id, i) => ids[i + 1] - id)
 
 console.log(ids[diff.findIndex(x => x !== 1)] + 1)
 
-function id ([...input]) {
-  const [row, col] = [127, 7].map((max, i) =>
-    find(0, max, i ? input : input.splice(0, 7))
-  )
-  return row * 8 + col
-}
-
-function find (min, max, steps) {
-  const reduce = min + ((max - min) >> 1)
-  if (min === max) return min
-  return find(
-    ...(/F|L/.test(steps.shift()) ? [min, reduce] : [reduce + 1, max]),
-    steps
-  )
+function id (input) {
+  return parseInt(input.replace(/(F|L)/g, '0').replace(/\D/g, '1'), 2)
 }
--- 2020/day-5.js
+++ 2020/day-5.js
@@ -2,18 +2,6 @@ const input = require('./read')(__filename)
 
 console.log(Math.max(...[...input].map(id)))
 
-function id ([...input]) {
-  const [row, col] = [127, 7].map((max, i) =>
-    find(0, max, i ? input : input.splice(0, 7))
-  )
-  return row * 8 + col
-}
-
-function find (min, max, steps) {
-  const reduce = min + ((max - min) >> 1)
-  if (min === max) return min
-  return find(
-    ...(/F|L/.test(steps.shift()) ? [min, reduce] : [reduce + 1, max]),
-    steps
-  )
+function id (input) {
+  return parseInt(input.replace(/(F|L)/g, '0').replace(/\D/g, '1'), 2)
 }
