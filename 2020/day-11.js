610131e98b6d67e36c0f7735a61297135f2aa580
James Kyburz <james.kyburz@gmail.com>
2020-12-15 09:10:56 +0000

    day 11 part 1

---
+++ 2020/day-11.js
@@ -0,0 +1,63 @@
+const rows = [...require('./read')(__filename)].map(x => [...x])
+const readline = require('readline')
+
+const { stdout } = process
+
+const cursorTo = (x, y) => readline.cursorTo(stdout, x, y)
+const clearScreenDown = () => readline.clearScreenDown(stdout)
+
+cursorTo(0, 0)
+clearScreenDown()
+
+run().catch(console.error)
+
+async function run () {
+  while (true) {
+    const changes = []
+    clearScreenDown()
+
+    for (const y of [...Array(rows.length).keys()]) {
+      cursorTo(0, y)
+      for (const x of [...Array(rows[y].length).keys()]) {
+        if (rows[y][x] === 'L' && occupied(x, y) === 0) {
+          changes.push([x, y, '#'])
+        } else if (rows[y][x] === '#' && occupied(x, y) > 3) {
+          changes.push([x, y, 'L'])
+        }
+        stdout.write(rows[y][x])
+      }
+    }
+
+    console.log()
+
+    if (changes.length) {
+      for (const [x, y, value] of changes) {
+        rows[y][x] = value
+      }
+    } else {
+      break
+    }
+    await new Promise(resolve => setTimeout(resolve, 200))
+  }
+
+  console.log(
+    'occupied',
+    rows.reduce(
+      (sum, cols) => sum + cols.filter(value => value === '#').length,
+      0
+    )
+  )
+}
+
+function occupied (x, y) {
+  return [
+    [x - 1, y - 1],
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x - 1, y],
+    [x + 1, y],
+    [x - 1, y + 1],
+    [x, y + 1],
+    [x + 1, y + 1]
+  ].filter(([x, y]) => rows[y] && rows[y][x] === '#').length
+}
